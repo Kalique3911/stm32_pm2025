64dc1c8e853dfd329673881f464386d1b84caf9b
Kalique <kalique@gmail.com>
2025-12-10 01:58:52 +0200

    feat(main): add lab 3

--- main.c
+++ main.c
@@ -1,38 +1,64 @@
 #include <stdint.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/timer.h>
-#include <libopencmsis/core_cm3.h>
+#include <stm32f10x.h>
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
-		__asm("nop");
-	}
+volatile uint32_t msTicks = 0;
+
+void SysTick_Handler(void) {
+    msTicks++;
 }
 
-void TIM2_IRQHandler(void) {
-//void tim2_isr(void) {
-   gpio_toggle(GPIOC, GPIO13);
-   timer_clear_flag(TIM2, TIM_SR_UIF);
+void delay_ms(uint32_t ms) {
+    uint32_t start = msTicks;
+    while ((msTicks - start) < ms);
+}
+
+void wait_button_release(uint32_t pin) {
+    while (!(GPIOA->IDR & (1U << pin))) {
+    }
+    delay_ms(20);
 }
 
 int __attribute((noreturn)) main(void) {
-rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
-rcc_periph_clock_enable(RCC_GPIOC);
-gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    SystemInit();
+    SysTick_Config(SystemCoreClock / 1000);
 
-rcc_periph_clock_enable(RCC_TIM2);
-rcc_periph_reset_pulse(RST_TIM2);
-timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-timer_set_prescaler(TIM2, 719);
-timer_disable_preload(TIM2);
-timer_set_period(TIM2, 50000);
-timer_enable_irq(TIM2, TIM_DIER_UIE);
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 
-nvic_enable_irq(NVIC_TIM2_IRQ);
-nvic_clear_pending_irq(NVIC_TIM2_IRQ);
-timer_enable_counter(TIM2);
+    GPIOC->CRH &= ~GPIO_CRH_CNF13;
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;
 
-while (1) {__asm volatile ("nop");}
+    GPIOA->CRL &= ~((GPIO_CRL_MODE0 | GPIO_CRL_CNF0) |
+                    (GPIO_CRL_MODE1 | GPIO_CRL_CNF1));
+    GPIOA->CRL |= (GPIO_CRL_CNF0_1 | GPIO_CRL_CNF1_1);
+    GPIOA->ODR |= (1U << 0) | (1U << 1);
 
+    float freq_factor = 1.0f;
+    const float MAX_FREQ = 64.0f;
+    const float MIN_FREQ = 1.0f / 64.0f;
+
+    uint32_t base_period_ms = 1000;
+    uint32_t current_period_ms = base_period_ms;
+
+    while (1) {
+        GPIOC->ODR ^= (1U << 13);
+        delay_ms(current_period_ms);
+
+        if (!(GPIOA->IDR & (1U << 0))) {
+            wait_button_release(0);
+            if (freq_factor < MAX_FREQ) {
+                freq_factor *= 2.0f;
+                current_period_ms = base_period_ms / freq_factor;
+            }
+        }
+
+        if (!(GPIOA->IDR & (1U << 1))) {
+            wait_button_release(1);
+            if (freq_factor > MIN_FREQ) {
+                freq_factor /= 2.0f;
+                current_period_ms = base_period_ms / freq_factor;
+            }
+        }
+    }
 }
